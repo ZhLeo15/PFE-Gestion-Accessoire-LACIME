e9f153657e64b217312bbd6c2f5d585e77b5439d
antoine <antoine.marcotte.2@ens.etsmtl.ca>
2025-03-24 13:33:07 -0400

    ajout des répertoire docker Snipe-IT et HomeAssistant + scripts

---
+++ docker/homeassistant/webhook_snipeit_to_homeassistant.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, Request
+#from parse import parse
+import aiohttp
+import uvicorn
+import re
+
+# === CONFIGURATION ===
+
+HA_URL = "http://localhost:8123"
+HA_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIzODhmOTlhZTM1NWI0ZThjOWQ3OWM4NGQyM2FmZjhhZCIsImlhdCI6MTc0MjY4NzczMCwiZXhwIjoyMDU4MDQ3NzMwfQ.CvUC4ISQ1u95DZ8kZXW7WGNEMtILBJtA88Mr8tW4vmU"
+
+# États cibles
+STATE_ON = "checked out"
+STATE_OFF = "checked in"
+
+# === APP FASTAPI ===
+
+app = FastAPI()
+
+async def set_switch(state: str, id: str):
+    url = f"{HA_URL}/api/services/switch/turn_{state}"
+    headers = {
+        "Authorization": f"Bearer {HA_TOKEN}",
+        "Content-Type": "application/json"
+    }
+    payload = {"entity_id": f"switch.asset_{id}"}
+    print(f"switch.asset_{id}")
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, json=payload) as response:
+            response.raise_for_status()
+            print(f"[HA] switch.switch.asset_{id} -> {state}")
+
+@app.post("/webhook")
+async def receive_webhook(request: Request):
+    payload = await request.json()
+    #print(f"[Webhook reçu] ID : {payload}")
+
+    try:
+        title_link = payload["attachments"][0]["title_link"]  # ex: http://.../hardware/1
+        match = re.search(r"/hardware/(\d+)", title_link)
+        if match:
+            hardware_id = match.group(1).zfill(5)
+
+            print(f"[INFO] Hardware ID : {hardware_id}")
+
+            if payload.get("text", {}).find(STATE_ON) != -1:
+                await set_switch("on", hardware_id)
+                print("[Webhook reçu] Nouveau statut : checked out")
+            elif payload.get("text", {}).find(STATE_OFF) != -1:
+                await set_switch("off", hardware_id)
+                print("[Webhook reçu] Nouveau statut : checked in")
+            else:
+                print("[INFO] Statut ignoré")
+        else:
+            print("[ERREUR] ID non trouvé dans title_link")
+            return {"status": "erreur"}
+    except Exception as e:
+        print(f"[ERREUR] Webhook mal formé ou données manquantes : {e}")
+
+    return {"status": "ok"}
